68766789e32d51c59703ca6e9075200babf3f45d
Ranieri93 <ranieri.innocenti@gmil.com>
2020-02-06 12:45:50 +0100

    esercizio completato.2

--- index.php
+++ index.php
@@ -6,14 +6,27 @@ class Prodotto {
     public $nome;
     public $idInterno;
     public $prezzo;
+
+    public function __construct($_name, $_prezzo)
+    {
+        $this->nome = $_name;
+        $this->idInterno = self::generacodice();
+        $this->prezzo = $_prezzo;
+    }
+
+    public static function generacodice() {
+        return random_int(1,100);
+    }
+
 }
 
-$chiantiRiserva = new Prodotto ();
-$chiantiRiserva->nome = 'le bocce';
-$chiantiRiserva->idInterno = 5;
-$chiantiRiserva->prezzo = 20;
+
+
+$chiantiRiserva = new Prodotto ('Le Bocce','532');
+
 
 echo '<pre>';
 var_dump($chiantiRiserva);
 echo '</pre>';
+
 
--- tipologia.php
+++ tipologia.php
@@ -1,32 +1,40 @@
 <?php
 include_once 'index.php';
-
 class tipologia extends prodotto {
     public $uvaggio;
     public $vinificazione;
     public $affinamento;
 
-    public function __construct($_uvaggio, $_vinificazione, $_affinamento , $_nome, $_idInterno = null, $_prezzo)
+    public function __construct($_uvaggio, $_vinificazione, $_affinamento,$_nome,$_prezzo)
     {
+        parent::__construct($_nome,$_prezzo);
         $this->uvaggio = $_uvaggio;
         $this->vinificazione = $_vinificazione;
         $this->affinamento = $_affinamento;
-        $this->nome = $_nome;
-        $this->idInterno = $_idInterno;
-        $this->prezzo = $_prezzo;
     }
 
-    public function getMyWine() {
-        echo 'nome vino: ' . $this->nome . '<br>' . 'prezzo vino: ' . $this->prezzo . '<br>' . 'uvaggio: ' . $this->uvaggio . '<br>' . 'vinificazione: ' . $this->vinificazione . '<br>' . 'affinamento: ' . $this->affinamento . '<br>';
+    public function setMyWine() {
+        return 'nome vino: ' . $this->nome .
+            '<br>' . 'prezzo vino: ' . $this->prezzo .
+            '<br>' . 'uvaggio: ' . $this->uvaggio .
+            '<br>' . 'vinificazione: ' . $this->vinificazione .
+            '<br>' . 'affinamento: ' . $this->affinamento . '<br>';
     }
+
 }
+
+
 
 $masseto = new tipologia('merlot','acciaio','legno', 'Masseto', 25,500);
 
 $sassicaia = new tipologia('CabFranc', 'acciaio','legno','Sassicaia',26, 150);
-//echo '<pre>';
-//var_dump($masseto);
-//echo '</pre>';
 
-echo $masseto->getMyWine();
-echo $sassicaia->getMyWine();
+echo '<pre>';
+var_dump($masseto);
+var_dump($sassicaia);
+echo '</pre>';
+
+
+
+echo $masseto->setMyWine();
+echo $sassicaia->setMyWine();
